3aafeccb6a871e62a7e3e5d98803217475501d22
Manny Lou <mavkio1@gmail.com>
2020-10-25 22:24:17 -0400

    Added HTML Routes and updated issues with API Routes and Workout.JS

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,53 @@
-const workout = require("../models/Workout")
+const Workout = require('../models/workout');
 
-
-module.exports = function (app) {
-  app.get("/api/workouts", (req, res) => {
-    workout.find({})
-        .then(dbWorkout => {
-          res.json(dbWorkout);
+module.exports = function(app) {
+  app.get('/api/workouts', function(req, res) {
+    Workout.find({})
+        .then((data) =>{
+          res.json(data);
         })
-        .catch(err => {
+        .catch((err) => {
           res.json(err);
         });
   });
 
 
-}
+  app.post('/api/workouts', function(req, res) {
+    Workout.create({})
+        .then((data) => res.json(data))
+        .catch((err) => {
+          res.json(err);
+        });
+  });
+
+  app.get('/api/workouts/range', function(req, res) {
+    Workout.find({})
+        .then((data) =>{
+          res.json(data);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+  });
+
+
+  app.post('/api/workouts/range', function(req, res) {
+    Workout.create({})
+        .then((data) => res.json(data))
+        .catch((err) => {
+          res.json(err);
+        });
+  });
+
+  app.put('/api/workouts/:id', ({body, params}, res)=>{
+    Workout.findByIdAndUpdate(
+        params.id,
+        {$push: {exercises: body}},
+        {new: true, runValidators: true},
+    )
+        .then((data) => res.json(data))
+        .catch((err) => {
+          res.json(err);
+        });
+  });
+};
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
-// const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-// const db = require("./models");
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +9,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true  });
 
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
